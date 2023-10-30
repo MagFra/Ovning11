4b4edcbf68cebb15945dda4a7d763c741610fd2d
MagFra <47413425+magfra@users.noreply.github.com>
2023-10-30 16:47:34 +0100

    Backup av dagen arbete.

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,15 +1,14 @@
 ﻿namespace Storage.Models
 {
-# nullable disable
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
         public DateTime Orderdate { get; set; }
-        public string Category { get; set; }
-        public string Shelf { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string? Shelf { get; set; }
         public int Count { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
